985f6c29c63abc44eceabc9e40ec41471e402015
AF_Askar <68832286+abo3skr2019@users.noreply.github.com>
2026-05-10 22:54:40 +0300

    increased coverage of mqtt

--- IoT/Mechanic/unit_tests/test_local_mqtt.c
+++ IoT/Mechanic/unit_tests/test_local_mqtt.c
@@ -64,6 +64,55 @@ void test_mqtt_init_uses_default_when_nvs_unavailable(void)
     TEST_ASSERT_EQUAL_STRING(MQTT_DEFAULT_BROKER_URI, broker);
 }
 
+void test_mqtt_init_loads_broker_from_nvs(void)
+{
+    nvs_set_str(1, MQTT_NVS_KEY_BROKER_URI, "mqtt://saved.example");
+
+    TEST_ASSERT_EQUAL(ESP_OK, mqtt_module_init());
+
+    char broker[MQTT_BROKER_URI_MAX_LEN] = {0};
+    TEST_ASSERT_TRUE(mqtt_module_get_broker_uri(broker, sizeof(broker)));
+    TEST_ASSERT_EQUAL_STRING("mqtt://saved.example", broker);
+}
+
+void test_mqtt_init_falls_back_on_invalid_nvs_value(void)
+{
+    nvs_set_str(1, MQTT_NVS_KEY_BROKER_URI, "");
+
+    TEST_ASSERT_EQUAL(ESP_OK, mqtt_module_init());
+
+    char broker[MQTT_BROKER_URI_MAX_LEN] = {0};
+    TEST_ASSERT_TRUE(mqtt_module_get_broker_uri(broker, sizeof(broker)));
+    TEST_ASSERT_EQUAL_STRING(MQTT_DEFAULT_BROKER_URI, broker);
+}
+
+void test_mqtt_vehicle_id_fallback_uses_random_once(void)
+{
+    esp_random_stub_set_value(0xDEADBEEFu);
+
+    const char *first = mqtt_get_vehicle_id_locked();
+    const char *second = mqtt_get_vehicle_id_locked();
+
+    TEST_ASSERT_EQUAL_STRING("vehicle_deadbeef", first);
+    TEST_ASSERT_EQUAL_STRING("vehicle_deadbeef", second);
+}
+
+void test_mqtt_build_topic_rejects_invalid_args(void)
+{
+    char topic[16] = {0};
+
+    TEST_ASSERT_EQUAL(ESP_ERR_INVALID_ARG, mqtt_build_topic_locked(NULL, topic, sizeof(topic)));
+    TEST_ASSERT_EQUAL(ESP_ERR_INVALID_ARG, mqtt_build_topic_locked(MQTT_TOPIC_DATA_SUFFIX, NULL, sizeof(topic)));
+    TEST_ASSERT_EQUAL(ESP_ERR_INVALID_ARG, mqtt_build_topic_locked(MQTT_TOPIC_DATA_SUFFIX, topic, 0));
+}
+
+void test_mqtt_build_topic_rejects_truncated_buffer(void)
+{
+    char topic[8] = {0};
+
+    TEST_ASSERT_EQUAL(ESP_ERR_INVALID_SIZE, mqtt_build_topic_locked(MQTT_TOPIC_DATA_SUFFIX, topic, sizeof(topic)));
+}
+
 void test_mqtt_set_broker_uri_rejects_invalid(void)
 {
     mqtt_module_init();
@@ -88,12 +137,75 @@ void test_mqtt_set_and_get_broker_uri(void)
     TEST_ASSERT_EQUAL_STRING("mqtt://example.com", nvs_stub_get_last_set_value());
 }
 
+void test_mqtt_set_broker_uri_restarts_on_change(void)
+{
+    mqtt_module_init();
+    wifi_stub_set_connected(true);
+    mqtt_start_client_locked();
+    mqtt_client_stub_reset();
+
+    TEST_ASSERT_EQUAL(ESP_OK, mqtt_module_set_broker_uri("mqtt://new-broker"));
+
+    TEST_ASSERT_EQUAL_INT(1, mqtt_client_stub_get_stop_calls());
+    TEST_ASSERT_EQUAL_INT(1, mqtt_client_stub_get_destroy_calls());
+    TEST_ASSERT_EQUAL_INT(1, mqtt_client_stub_get_start_calls());
+    TEST_ASSERT_EQUAL_STRING("mqtt://new-broker", mqtt_client_stub_get_last_uri());
+}
+
+void test_mqtt_set_broker_uri_does_not_restart_on_commit_failure(void)
+{
+    mqtt_module_init();
+    wifi_stub_set_connected(true);
+    mqtt_start_client_locked();
+    mqtt_client_stub_reset();
+    nvs_stub_set_commit_result(ESP_FAIL);
+
+    TEST_ASSERT_EQUAL(ESP_FAIL, mqtt_module_set_broker_uri("mqtt://other"));
+
+    TEST_ASSERT_EQUAL_INT(0, mqtt_client_stub_get_stop_calls());
+    TEST_ASSERT_EQUAL_INT(0, mqtt_client_stub_get_destroy_calls());
+    TEST_ASSERT_EQUAL_INT(0, mqtt_client_stub_get_start_calls());
+}
+
 void test_mqtt_publish_dtc_requires_client(void)
 {
     mqtt_module_init();
 
     TEST_ASSERT_EQUAL(ESP_ERR_INVALID_ARG, mqtt_module_publish_dtc("VIN", NULL));
     TEST_ASSERT_EQUAL(ESP_ERR_INVALID_STATE, mqtt_module_publish_dtc("VIN", "payload"));
+}
+
+void test_mqtt_start_client_locked_requires_wifi(void)
+{
+    mqtt_start_client_locked();
+
+    TEST_ASSERT_EQUAL_INT(0, mqtt_client_stub_get_start_calls());
+    TEST_ASSERT_EQUAL_INT(0, mqtt_client_stub_get_publish_calls());
+}
+
+void test_mqtt_start_client_locked_publishes_status(void)
+{
+    wifi_stub_set_connected(true);
+
+    mqtt_start_client_locked();
+
+    TEST_ASSERT_EQUAL_INT(1, mqtt_client_stub_get_start_calls());
+    TEST_ASSERT_EQUAL_INT(1, mqtt_client_stub_get_publish_calls());
+    TEST_ASSERT_NOT_NULL(strstr(mqtt_client_stub_get_last_topic(), "/status"));
+    TEST_ASSERT_EQUAL_STRING("online", mqtt_client_stub_get_last_payload());
+}
+
+void test_mqtt_stop_client_locked_stops_and_destroys(void)
+{
+    wifi_stub_set_connected(true);
+    mqtt_start_client_locked();
+    mqtt_client_stub_reset();
+
+    mqtt_stop_client_locked();
+
+    TEST_ASSERT_EQUAL_INT(1, mqtt_client_stub_get_stop_calls());
+    TEST_ASSERT_EQUAL_INT(1, mqtt_client_stub_get_destroy_calls());
+    TEST_ASSERT_NULL(s_client);
 }
 
 void test_mqtt_publish_dtc_publishes_topic_and_payload(void)
@@ -111,15 +223,45 @@ void test_mqtt_publish_dtc_publishes_topic_and_payload(void)
     TEST_ASSERT_EQUAL_STRING("payload", mqtt_client_stub_get_last_payload());
 }
 
+void test_mqtt_module_set_vin_validates_input(void)
+{
+    TEST_ASSERT_EQUAL(ESP_ERR_INVALID_ARG, mqtt_module_set_vin("VIN"));
+
+    mqtt_module_init();
+
+    TEST_ASSERT_EQUAL(ESP_ERR_INVALID_ARG, mqtt_module_set_vin(""));
+}
+
+void test_mqtt_module_set_vin_updates_vehicle_id(void)
+{
+    mqtt_module_init();
+
+    TEST_ASSERT_EQUAL(ESP_OK, mqtt_module_set_vin("VIN555"));
+
+    TEST_ASSERT_EQUAL_STRING("VIN555", mqtt_get_vehicle_id_locked());
+}
+
 int main(void)
 {
     UNITY_BEGIN();
 
     RUN_TEST(test_mqtt_init_uses_default_when_nvs_unavailable);
+    RUN_TEST(test_mqtt_init_loads_broker_from_nvs);
+    RUN_TEST(test_mqtt_init_falls_back_on_invalid_nvs_value);
+    RUN_TEST(test_mqtt_vehicle_id_fallback_uses_random_once);
+    RUN_TEST(test_mqtt_build_topic_rejects_invalid_args);
+    RUN_TEST(test_mqtt_build_topic_rejects_truncated_buffer);
     RUN_TEST(test_mqtt_set_broker_uri_rejects_invalid);
     RUN_TEST(test_mqtt_set_and_get_broker_uri);
+    RUN_TEST(test_mqtt_set_broker_uri_restarts_on_change);
+    RUN_TEST(test_mqtt_set_broker_uri_does_not_restart_on_commit_failure);
     RUN_TEST(test_mqtt_publish_dtc_requires_client);
+    RUN_TEST(test_mqtt_start_client_locked_requires_wifi);
+    RUN_TEST(test_mqtt_start_client_locked_publishes_status);
+    RUN_TEST(test_mqtt_stop_client_locked_stops_and_destroys);
     RUN_TEST(test_mqtt_publish_dtc_publishes_topic_and_payload);
+    RUN_TEST(test_mqtt_module_set_vin_validates_input);
+    RUN_TEST(test_mqtt_module_set_vin_updates_vehicle_id);
 
     return UNITY_END();
 }
